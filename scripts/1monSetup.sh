c4e1d0121124af7cb9c5fd6717a3415b48652b8b
FatAndreasbot <86000785+FatAndreasbot@users.noreply.github.com>
2024-08-03 09:23:35 +0700

    i did an oopsie

--- scripts/1monSetup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-killall waybar
-
-hyprctl keyword monitor "eDP-1, 1920x1080@60, 0x350, 1"
-
-waybar -c $HOME/.config/waybar/config &
--- scripts/screenshot.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-area=$(slurp)
-
-
-grim -g "$area" "/home/andreasbot/Pictures/Screenshots/$(echo screenshot_$(date).png)"
-
-grim -g "$area" - | wl-copy
